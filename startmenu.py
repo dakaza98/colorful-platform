cce79bbeb6732bcb4e935db2c45624a9f99424ff
Daniel Fehrm <daniel@fehrm.se>
2020-02-06 15:23:51 +0100

    users can now start or quit the game

--- startmenu.py
+++ startmenu.py
@@ -8,6 +8,16 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+class choices:
+    # Choices must be lowercase
+    START = "s"
+    QUIT = "q"
+
+    @staticmethod
+    def is_valid_choice(choice):
+        choice = choice.lower()
+        return choice == choices.START or choice == choices.QUIT
+
 starttext = open("s.txt", 'r')
 quittext = open('q.txt', 'r')
 title = open('title.txt', 'r')
@@ -16,3 +26,15 @@ print(bcolors.OKGREEN + starttext.read())
 print(bcolors.FAIL + quittext.read())
 print(bcolors.ENDC)
 
+while True:
+    choice = input("Press S to start or Q to quit\n")
+
+    if choices.is_valid_choice(choice):
+        if choice == choices.START:
+            # Start game here
+            print("Game will now start")
+            break
+        elif choice == choices.QUIT:
+            break
+        else:
+            raise Exception("Choice " + choice + " has no implementation")
