833efe4067c24eb7eae2f615db784ab610bb4b66
Daniel Fehrm <daniel@fehrm.se>
2020-02-07 12:18:58 +0100

    Remade start menu with curses

--- startmenu.py
+++ startmenu.py
@@ -1,44 +1,60 @@
-class choices:
-    # Choices must be lowercase
-    START = "s"
-    QUIT = "q"
+import curses
+import time
 
-    @staticmethod
-    def is_valid_choice(choice):
-        choice = choice.lower()
-        return choice == choices.START or choice == choices.QUIT
+menu = ["start", "quit"]
 
-def print_title_screen():
-    class bcolors:
-        PURPLE = '\033[95m'
-        BLUE = '\033[94m'
-        GREEN = '\033[92m'
-        YELLOW = '\033[93m'
-        RED = '\033[91m'
-        ENDCOLOR = '\033[0m'
-        BOLD = '\033[1m'
-        UNDERLINE = '\033[4m'
+title = open('title.txt', 'r')
+titletext = title.read()
+title.close()
 
-    starttext = open("start.txt", 'r')
-    quittext = open('quit.txt', 'r')
-    title = open('title.txt', 'r')
+def print_menu(screen, selected_row_index):
+    screen.clear()
 
-    print(bcolors.YELLOW + title.read())
-    print(bcolors.GREEN + starttext.read())
-    print(bcolors.RED + quittext.read())
-    print(bcolors.ENDCOLOR)
+    num_rows, num_cols = screen.getmaxyx()
 
-print_title_screen()
+    splitted_title = titletext.split("\n")
+    x = int(num_cols / 2) - int(len(splitted_title[0]) / 2)
+    for index, line in enumerate(splitted_title):
+        screen.addstr(index + 2, x, splitted_title[index])
 
-while True:
-    choice = input("Press S to start or Q to quit\n")
-
-    if choices.is_valid_choice(choice):
-        if choice == choices.START:
-            # Start game here
-            print("Game will now start")
-            break
-        elif choice == choices.QUIT:
-            break
+    for index, row in enumerate(menu):
+        x = int(num_cols / 2) - int(len(row) / 2)
+        y = int(num_rows / 2) - len(menu) + index
+        if index == selected_row_index:
+            screen.attron(curses.color_pair(1))
+            screen.addstr(y, x, row.capitalize())
+            screen.attroff(curses.color_pair(1))
         else:
-            raise Exception("Choice " + choice + " has no implementation")
+            screen.addstr(y, x, row.capitalize())
+
+    screen.refresh()
+
+def main(screen):
+    # Disable blinking cursor
+    curses.curs_set(0)  
+
+    current_row = 0
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    print_menu(screen, current_row)
+
+    while True:
+        pressed_key = screen.getch()
+
+        if pressed_key == curses.KEY_UP and current_row > 0:
+            current_row -= 1
+        elif pressed_key == curses.KEY_DOWN and current_row < len(menu) - 1:
+            current_row += 1
+        # KEY_ENTER might not always work because some computers sends the value 10 or 13 instead
+        elif pressed_key == curses.KEY_ENTER or pressed_key in [10, 13]:
+            if current_row == 0:
+                screen.clear()
+                screen.addstr(0, 0, "Game will now start")
+                screen.refresh()
+                time.sleep(3)
+                break
+            elif current_row == 1:
+                break
+
+        print_menu(screen, current_row)
+
+curses.wrapper(main)
